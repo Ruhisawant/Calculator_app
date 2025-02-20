ba4338ebf35dc0dae1de12b8dca522d48ad26f8c
Ruhisawant <ruhi.p.sawant@gmail.com>
2025-02-20 16:14:49 -0500

    Added display

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -29,6 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> buttonNames = [];
 
   @override
   Widget build(BuildContext context) {
@@ -40,41 +42,58 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: SizedBox(
-            width: 500,
-            height: 500,
-            child: GridView.builder(
-              shrinkWrap: true,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 4,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Display the list of button names, one on each line
+              Text(
+                buttonNames.isEmpty
+                    ? 'Press a button'
+                    : buttonNames.join(''),
+                style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
-              itemCount: 16,
-              itemBuilder: (context, index) {
-                return InkWell(
-                  onTap: () {
-                    Text('Bt $index pressed');
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.brown,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    alignment: Alignment.center,
-                    height: 80,
-                    width: 80,
-                    child: Text(
-                      'Btn $index',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
+              const SizedBox(height: 20),
+
+              // Grid of buttons
+              SizedBox(
+                width: 500,
+                height: 500,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
                   ),
-                );
-              },
-            ),
+                  itemCount: 16,
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: () {
+                        setState(() {
+                          buttonNames.add('$index');
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.brown,
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        alignment: Alignment.center,
+                        height: 80,
+                        width: 80,
+                        child: Text(
+                          '$index',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
